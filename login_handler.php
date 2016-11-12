222b84c0cbbd92e9b5854f769774e29cfcc34f3d
Ryan Hawkins <ryanhawkins@u.boisestate.edu>
2016-11-11 21:40:01 -0700

    trying to get error messages to save and work

--- login_handler.php
+++ login_handler.php
@@ -4,7 +4,8 @@ $email = trim($_POST['email']);
 $password = trim($_POST['password']);
 
 
-
+	
+	/**
 	if(strlen($email) <= 10 || strlen($email) > 256){
 		$emailError = "Invalid Email Address Size";
 	
@@ -14,7 +15,7 @@ $password = trim($_POST['password']);
 	if(isempty($password) || strlen($password) > 100){
 		$passwordSizeError = "Too small or too big of an email";
 	}
-
+**/
 	
 	/**
 	if(0 === preg_match('/^.+@.+\.[A-Za-z]{1,5}$/', $email, $matches)){
