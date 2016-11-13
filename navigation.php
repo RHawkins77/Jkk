a9ca807bf80d08f2d236ca3940e5178928a8ed76
Ryan Hawkins <ryanhawkins@u.boisestate.edu>
2016-11-12 18:23:49 -0700

    working on navigation

--- navigation.php
+++ navigation.php
@@ -1,25 +1,45 @@
+
+
+
 <div id ="navigation">
 <ul>
-	<li<?php if ($thisPage=="Home") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="index.html">Home</a>|</li>
-	<li<?php if ($thisPage=="HeadWare") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="HeadWare.html">HeadWare</a>|</li>
-	<li<?php if ($thisPage=="Scarfs") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="Scarfs.html">Scarfs</a>|</li>
-	<li<?php if ($thisPage=="Gloves") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="Gloves.html">Gloves</a>|</li>
-	<li<?php if ($thisPage=="Scrubbies") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="Scrubbies.html">Scrubbies</a>|</li>
-	<li<?php if ($thisPage=="Shawls") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="Shawls.html">Shawls</a>|</li>
-	<li<?php if ($thisPage=="Purses") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="Purses.html">Purses</a>|</li>
-	<li<?php if ($thisPage=="ProdInfo") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="ProdInfo.html">Product Information</a>|</li>
-	<li<?php if ($thisPage=="UpcomingEvents") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="UpcomingEvents.html">Upcoming Events</a>|</li>
-	<li<?php if ($thisPage=="ContactUs") echo " id=\"currentpage\""; ?>>
+	<li>
 	<a href="ContactUs.html">Contact Us</a></li>
 </ul>
 </div>
 
+<?php 
+/**<?php if ($thisPage=="Home") echo " id=\"currentpage\""; ?>
+i<?php if ($thisPage=="HeadWare") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="Scarfs") echo " id=\"currentpage\""; ?>
+
+
+<?php if ($thisPage=="Gloves") echo " id=\"currentpage\""; ?>
+
+
+<?php if ($thisPage=="Scrubbies") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="Shawls") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="Purses") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="ProdInfo") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="UpcomingEvents") echo " id=\"currentpage\""; ?>
+<?php if ($thisPage=="ContactUs") echo " id=\"currentpage\""; ?>
+**/
+?>
